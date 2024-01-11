06f928e21930e69ede729467223ba7e47614e58a
Jose Rivera <jay@Joses-MacBook-Pro.local>
2024-01-10 23:16:19 -0800

    added email integration to recive emails

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# E-PORTFOLIO
-# E-PORTFOLIO
+
--- index.js
+++ index.js
@@ -2,8 +2,27 @@
 // service_cf2pcf4
 // srU5YOcDS1ay8lnZC
 
-function contact() {
-    console.log(`IT WORKED!!`)
-}
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(`.modal__overlay--loading`);
+  const success = document.querySelector(`.modal__overlay--success`);
+  loading.classList += " modal__overlay--visible";
 
-// test
+  emailjs
+    .sendForm(
+      "service_cf2pcf4",
+      "template_ujyba8h",
+      event.target,
+      "srU5YOcDS1ay8lnZC"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporaly unavailable. Please contact me directly at w.jayrivera@gmail.com."
+      );
+    });
+}
